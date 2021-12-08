7e5180c3ef8bc40d4adc7612f2f4b2277fbe6177
Erik Thorsell <erik@thorsell.io>
2021-12-08 08:29:51 +0100

    Add solution to part 2, Day 8, 2021

--- src/advent_of_code/y2021/d8.py
+++ src/advent_of_code/y2021/d8.py
@@ -1,13 +1,11 @@
 """Solution module for Day X, YEAR"""
 import copy
 from collections import defaultdict
-from itertools import permutations
 import time
-from sys import maxsize
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from advent_of_code.utils.fetch import fetch
-from advent_of_code.utils.parse import parse_seven_segment, split_number_by_newline
+from advent_of_code.utils.parse import parse_seven_segment
 
 
 def determine_digit_by_length(signal: str) -> int:
@@ -22,7 +20,7 @@ def determine_digit_by_length(signal: str) -> int:
     return -1
 
 
-def solution_1(input):
+def solution_1(input: List[Tuple[str, str]]) -> int:
     occ = defaultdict(lambda: 0)
     for _, output in input:
         for digit in output.split():
@@ -30,8 +28,83 @@ def solution_1(input):
     return occ[1] + occ[4] + occ[7] + occ[8]
 
 
+def overlap(d1, d2: str) -> int:
+    return len(set(d1) & set(d2))
+
+
 def solution_2(input):
-    pass
+    output_values = 0
+    for signal, output in input:
+        digit_to_segments = {0: "", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "", 9: ""}
+        while any(digit == "" for digit in digit_to_segments.values()):
+            for digit in signal.split():
+                sd = "".join(sorted(digit))
+
+                # 0
+                if (
+                    len(sd) == 6
+                    and overlap(digit, digit_to_segments[8]) == 6
+                    and overlap(digit, digit_to_segments[2]) == 4
+                    and overlap(digit, digit_to_segments[5]) == 4
+                ):
+                    digit_to_segments[0] = sd
+
+                # 1
+                if len(sd) == 2:
+                    digit_to_segments[1] = sd
+
+                # 2
+                if (
+                    len(sd) == 5
+                    and overlap(digit, digit_to_segments[3]) == 4
+                    and overlap(digit, digit_to_segments[4]) == 2
+                ):
+                    digit_to_segments[2] = sd
+
+                # 3
+                if len(sd) == 5 and overlap(digit, digit_to_segments[1]) == 2:
+                    digit_to_segments[3] = sd
+
+                # 4
+                if len(sd) == 4:
+                    digit_to_segments[4] = sd
+
+                # 5
+                if (
+                    len(sd) == 5
+                    and overlap(digit, digit_to_segments[3]) == 4
+                    and overlap(digit, digit_to_segments[4]) == 3
+                ):
+                    digit_to_segments[5] = sd
+
+                # 6
+                if (
+                    len(sd) == 6
+                    and overlap(digit, digit_to_segments[4]) == 3
+                    and overlap(digit, digit_to_segments[7]) == 2
+                ):
+                    digit_to_segments[6] = sd
+
+                # 7
+                if len(sd) == 3:
+                    digit_to_segments[7] = sd
+
+                # 8
+                if len(sd) == 7:
+                    digit_to_segments[8] = sd
+
+                # 9
+                if len(sd) == 6 and overlap(digit, digit_to_segments[4]) == 4:
+                    digit_to_segments[9] = sd
+
+        str_output = ""
+        for digit in output.split():
+            sd = "".join(sorted(digit))
+            val_list = list(digit_to_segments.values())
+            str_output += str(val_list.index(sd))
+        output_values += int(str_output)
+
+    return output_values
 
 
 def run(year: int, day: int):
--- tests/y2021/test_2021_d8.py
+++ tests/y2021/test_2021_d8.py
@@ -22,6 +22,7 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
 
 def test_solution_2():
+    #    example_input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
     example_input = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
