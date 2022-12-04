a49e1d3a2450d3c836a9d57b5bd34b8b2f4930d8
Erik Thorsell <erik@thorsell.io>
2022-12-04 06:34:39 +0100

    Fix function name duplication

--- src/advent_of_code/utils/manhattan.py
+++ src/advent_of_code/utils/manhattan.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 
 
-def get_manhattan_distance_3d(x, y: Tuple[int, int]) -> int:
+def get_manhattan_distance(x, y: Tuple[int, int]) -> int:
     return abs(x[0] - y[0]) + abs(x[1] - y[1])
 
 
