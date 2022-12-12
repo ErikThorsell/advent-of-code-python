6827e6d76f98e921f0dcce9701bb8a6aa0710bc9
Erik Thorsell <erik@thorsell.io>
2022-12-12 10:15:21 +0100

    Add dijkstra solution for Day 12, 2022

--- src/advent_of_code/y2022/d12.py
+++ src/advent_of_code/y2022/d12.py
@@ -1,6 +1,7 @@
 """Solution module for Day 12, 2022"""
 from collections import deque
 import copy
+from heapq import heapify, heappop, heappush
 import time
 from sys import maxsize
 
@@ -39,6 +40,35 @@ def inbound(grid, r, c):
     return 0 <= r < rows and 0 <= c < cols
 
 
+def path_dijkstra(grid, sr, sc, er, ec):
+
+    distances = {}
+    distances[(sr, sc)] = 0
+    for rx in range(len(grid)):
+        for cx in range(len(grid[rx])):
+            if (rx, cx) != (sr, sc):
+                distances[(rx, cx)] = maxsize
+
+    heap = [(0, (sr, sc))]
+    heapify(heap)
+
+    while heap:
+        dist, (r, c) = heappop(heap)
+
+        if (r, c) == (er, ec):
+            return dist
+
+        for (nr, nc) in get_adjacent(r, c, 4):
+            if inbound(grid, nr, nc) and grid[nr][nc] - grid[r][c] <= 1:
+                n_dist = dist + 1
+                if distances[(nr, nc)] <= n_dist:
+                    continue
+                distances[(nr, nc)] = n_dist
+                heappush(heap, (n_dist, (nr, nc)))
+
+    return maxsize
+
+
 def path(grid, sr, sc, er, ec):
     rows, cols = len(grid), len(grid[0])
     steps = [[maxsize] * cols for _ in range(rows)]
@@ -57,7 +87,6 @@ def path(grid, sr, sc, er, ec):
 
 
 # Guesses for Part 1
-#  99 | Too large
 def solution_1(input):
     sr, sc = find(input, "S")[0]
     er, ec = find(input, "E")[0]
@@ -66,13 +95,30 @@ def solution_1(input):
 
 
 # Guesses for Part 2
-# 97 | Too small
 def solution_2(input):
     er, ec = find(input, "E")[0]
     grid = parse(input)
     min_dist = maxsize
     for (sr, sc) in find(input, "a"):
         min_dist = min(min_dist, path(grid, sr, sc, er, ec))
+    return min_dist
+
+
+# Guesses for Part 3
+def solution_3(input):
+    sr, sc = find(input, "S")[0]
+    er, ec = find(input, "E")[0]
+    grid = parse(input)
+    return path_dijkstra(grid, sr, sc, er, ec)
+
+
+# Guesses for Part 4
+def solution_4(input):
+    er, ec = find(input, "E")[0]
+    grid = parse(input)
+    min_dist = maxsize
+    for (sr, sc) in find(input, "a"):
+        min_dist = min(min_dist, path_dijkstra(grid, sr, sc, er, ec))
     return min_dist
 
 
@@ -91,3 +137,13 @@ def run(year: int, day: int):
     s2 = solution_2(copy.deepcopy(parsed_input))
     toc = time.perf_counter()
     print(f"Solution for problem 2: {s2}, acquired in: {toc-tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+    s3 = solution_3(copy.deepcopy(parsed_input))
+    toc = time.perf_counter()
+    print(f"Solution for problem 1 with Dijkstra: {s3}, acquired in: {toc-tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+    s4 = solution_4(copy.deepcopy(parsed_input))
+    toc = time.perf_counter()
+    print(f"Solution for problem 2 with Dijkstra: {s4}, acquired in: {toc-tic:0.4f} seconds")
