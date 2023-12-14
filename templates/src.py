a63a3279bbfb255a93b820595168dfd5f9c1578d
Erik Thorsell <erik@thorsell.io>
2023-12-14 08:57:48 +0100

    Include tests in template

--- templates/src.py
+++ templates/src.py
@@ -4,33 +4,54 @@ import time
 from sys import maxsize
 
 from advent_of_code.utils.fetch import fetch
-from advent_of_code.utils.parse import split_str_by_newline
+
+
+def test_1():
+    # Copy paste example here
+    input = """"""
+    answer = 0
+    assert(solution_1(input) == answer)
+    
+
+def test_2():
+    # Copy paste example here
+    input = """"""
+    answer = 0
+    assert(solution_2(input) == answer)
+
+
+def parse(input):
+    ...
 
 
 # Guesses for Part 1
 #  99 | Too large
 def solution_1(input):
-    pass
+    return 0
 
 
 # Guesses for Part 2
 # 97 | Too small
 def solution_2(input):
-    pass
+    return 0
 
 
 def run(year: int, day: int):
     print(f"\n🌟 Fetching input for {year}/{day} 🌟")
 
     input = fetch(year, day)
-    parsed_input = split_str_by_newline(input)
+
+    test_1()
+    print("Test 1 was successful!")
+    test_2()
+    print("Test 2 was successful!")
 
     tic = time.perf_counter()
-    s1 = solution_1(copy.deepcopy(parsed_input))
+    s1 = solution_1(copy.deepcopy(input))
     toc = time.perf_counter()
     print(f"Solution for problem 1: {s1}, acquired in: {toc-tic:0.4f} seconds")
 
     tic = time.perf_counter()
-    s2 = solution_2(copy.deepcopy(parsed_input))
+    s2 = solution_2(copy.deepcopy(input))
     toc = time.perf_counter()
     print(f"Solution for problem 2: {s2}, acquired in: {toc-tic:0.4f} seconds")
