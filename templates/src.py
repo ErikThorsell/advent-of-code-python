a59ac9e5909c916970ac6b7dd5812dded771afe6
Erik Thorsell <erik@thorsell.io>
2025-12-07 16:59:49 +0100

    Update template

--- templates/src.py
+++ templates/src.py
@@ -8,14 +8,15 @@ from advent_of_code.utils.fetch import fetch
 
 def test_1():
     input = """"""
-    answer = 0
-    assert(solution_1(input) == answer)
-    
+    expected_answer = 0
+    actual_answer = solution_1(input)
+    assert expected_answer == actual_answer, (f"Expected: {expected_answer}, got {actual_answer}")
 
 def test_2():
     input = """"""
-    answer = 0
-    assert(solution_2(input) == answer)
+    expected_answer = 0
+    actual_answer = solution_2(input)
+    assert expected_answer == actual_answer, (f"Expected: {expected_answer}, got {actual_answer}")
 
 
 def parse(input):
@@ -37,15 +38,15 @@ def run(year: int, day: int):
 
     test_1()
     print("Test 1 was successful!")
-    test_2()
-    print("Test 2 was successful!")
-
     tic = time.perf_counter()
     s1 = solution_1(copy.deepcopy(input))
     toc = time.perf_counter()
     print(f"Solution for problem 1: {s1}, acquired in: {toc-tic:0.4f} seconds")
 
+    test_2()
+    print("Test 2 was successful!")
     tic = time.perf_counter()
     s2 = solution_2(copy.deepcopy(input))
     toc = time.perf_counter()
     print(f"Solution for problem 2: {s2}, acquired in: {toc-tic:0.4f} seconds")
+
